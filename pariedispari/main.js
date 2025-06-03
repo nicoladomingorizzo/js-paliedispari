ef02eca86c3f9269ee8bdbb198badba1823d57f5
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-06-03 16:15:15 +0200

    Add input validation for user choice and number in pari e dispari game; created loop while for validation

--- pariedispari/main.js
+++ pariedispari/main.js
@@ -1,8 +1,18 @@
 /* Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto. */
 
 //  L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-const userChoice = prompt('Scegli pari o dispari'); // creo un prompt per permettere all'utente di scegliere pari o dispari
-const userNumber = Number(prompt('Scegli un numero da 1 a 5')); // creo un prompt per permettere all'utente di scegliere un numero da 1 a 5
+let userChoice = prompt('Scegli pari o dispari'); // creo un prompt per permettere all'utente di scegliere pari o dispari
+
+userChoice = userChoice.toLowerCase();
+
+while ((userChoice != 'pari') && (userChoice != 'dispari')) {
+    userChoice = prompt('Scegli pari o dispari');
+    //creo un ciclo while per far si che la parola scelta dall'utente sia pari o dispari
+}
+let userNumber = Number(prompt('Scegli un numero da 1 a 5')); // creo un prompt per permettere all'utente di scegliere un numero da 1 a 5
+while (userNumber <= 0 || userNumber > 5 || userNumber == NaN) {
+    userNumber = Number(prompt('Scegli un numero da 1 a 5'));
+} //creo un ciclo while per far si che il numero scelto dall'utente vada da 1 a 5
 
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione)
 function randomNumber() {
@@ -22,12 +32,16 @@ if (total(pcNumber, userNumber) % 2 === 0) {
 
 // Dichiariamo chi ha vinto.
 if ((userChoice == 'pari') && (total(pcNumber, userNumber) % 2 === 0)) {
+    // pari e pari
     alert('Complimenti, hai vinto');
 } else if ((userChoice == 'pari') && (total(pcNumber, userNumber) % 2 === 1)) {
+    // pari e dispari
     alert('Mi dipiace, hai perso');
 } else if ((userChoice == 'dispari') && (total(pcNumber, userNumber) % 2 === 0)) {
+    // dispari e pari
     alert('Mi dipiace, hai perso');
 } else {
+    // dispari e dispari
     alert('Complimenti, hai vinto');
 }
 // tramite un if/else, decretiamo se l'utente ha vinto o meno
