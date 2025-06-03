b92456f6cd414844f3c055daa71125059fce7d56
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-06-03 15:44:29 +0200

    Refactor pari e dispari game logic to improve clarity and correctness; fix random number generation and win condition checks

--- pariedispari/main.js
+++ pariedispari/main.js
@@ -6,25 +6,26 @@ const userNumber = Number(prompt('Scegli un numero da 1 a 5')); // creo un promp
 
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione)
 function randomNumber() {
-    return Math.floor(Math.random() * 5 + 1);
+    return Math.floor(Math.random() * 5) + 1;
 }
-alert(`Il computer ha estratto il numero ${randomNumber()}`);//inizializzo una funzione che crea un numero random da 1 a 5 
+const pcNumber = randomNumber()
+alert(`Il computer ha estratto il numero ${pcNumber}`);//inizializzo una funzione che crea un numero random da 1 a 5 
 // Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) 
 function total(num1, num2) {
     return num1 + num2;
 } // inizializziamo una funzione di somma tra num1 e num2
-if (total(((randomNumber()) + userNumber) % 2 === 0)) {
-    alert(`La somma dei numeri ${userNumber} e ${randomNumber()} è pari`)
+if (total(pcNumber, userNumber) % 2 === 0) {
+    alert(`La somma dei numeri ${userNumber} e ${pcNumber} è pari`)
 } else {
-    alert(`La somma dei numeri ${userNumber} e ${randomNumber()} è dispari`)
+    alert(`La somma dei numeri ${userNumber} e ${pcNumber} è dispari`)
 } // tramite un if/else, decretiamo se il numero è pari o dispari
 
 // Dichiariamo chi ha vinto.
-if ((userChoice == 'pari') == 'pari') {
+if ((userChoice == 'pari') && (total(pcNumber, userNumber) % 2 === 0)) {
     alert('Complimenti, hai vinto')
-} else if ((userChoice == 'pari') !== 'pari') {
+} else if ((userChoice == 'pari') && (total(pcNumber, userNumber) % 2 === 1)) {
     alert('Mi dipiace, hai perso')
-} else if ((userChoice == 'dispari') == 'pari') {
+} else if ((userChoice == 'dispari') && (total(pcNumber, userNumber) % 2 === 0)) {
     alert('Mi dipiace, hai perso')
 } else {
     alert('Complimenti, hai vinto')
