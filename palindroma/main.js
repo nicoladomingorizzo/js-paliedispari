6dac6a64a4941469fb2edc063e2d420042fcc19a
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-06-03 14:53:56 +0200

    Add initial HTML and JavaScript files for palindrome and even/odd games; update .gitignore; worked and finished palindrome

---
+++ palindroma/main.js
@@ -0,0 +1,21 @@
+/* Palidroma
+Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma */
+
+// Chiedere all’utente di inserire una parola
+const userWord = prompt('Scrivi una parola'); //creo una costante in cui inserisco un prompt
+
+
+function stringReverse(string) {
+    return string.split('').reverse().join('');
+} // creo una funzione in cui inserisco una concatenazione di metodi: per prima cosa inserisco uno split che trasforma la stringa in array dopo inserisco il reverse che inverte il flusso dell'array ed infine il join che ritrasforma l'array in stringa
+
+// Creare una funzione per capire se la parola inserita è palindroma
+function palindrome(userWord) {
+    if (userWord === stringReverse(userWord)) {
+        alert(`The word ${userWord} is : palindrome`);
+    } else {
+        alert(`The word ${userWord} isn't : palindrome`);
+    }
+} // scrivo una funziona che permette di verificare se la parola scritta è palindroma oppure no, usando la funzione creata in precedenza con il parametro interno che mi serve, ovvero il prompt che crea userWord
+
+palindrome(userWord);// infine richiamo la funzione che ho creato
