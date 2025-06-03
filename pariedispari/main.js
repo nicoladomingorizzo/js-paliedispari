9de9c301d3958e9e0fe66658a71d35006fe44857
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-06-03 15:00:49 +0200

    Add random number generation function for pari e dispari game

--- pariedispari/main.js
+++ pariedispari/main.js
@@ -5,7 +5,9 @@ const userChoice = prompt('Scegli pari o dispari'); // creo un prompt per permet
 const userNumber = Number(prompt('Scegli un numero da 1 a 5')); // creo un prompt per permettere all'utente di scegliere un numero da 1 a 5
 
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione)
-
+function randomNumber() {
+    return Math.floor(Math.random() * 5 + 1);
+} //inizializzo una funzione che crea un numero random da 1 a 5 
 // Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) 
 
 // Dichiariamo chi ha vinto.
